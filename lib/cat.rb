fae1bac8aa0ca6a05cae1d41500fc9964d7b9429
Daniel Lechok <dclechok@gmail.com>
2019-09-24 19:13:48 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,15 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = 'nervous'
   end
   
+  def self.all
+    
+  end
 end
