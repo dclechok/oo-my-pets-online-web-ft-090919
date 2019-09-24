b47dfe372e51237097932c9652b1a3cdc4eb8916
Daniel Lechok <dclechok@gmail.com>
2019-09-24 19:08:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,13 +2,20 @@ class Owner
   
   attr_reader :name, :species
   
+  @@all = []
+  
   def initialize(name = nil)
     @name = name
     @species = 'human'
+    
+    @@all << self
   end
   
   def say_species
     "I am a " + @species + "."
   end
    
+  def self.all
+    @all
+  end
 end
